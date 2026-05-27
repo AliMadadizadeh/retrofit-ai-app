d85d3d16cabad613f503d08edc64c2d6ebf70d77
Ali Madadizadeh <146396515+AliMadadizadeh@users.noreply.github.com>
2026-05-27 18:05:55 -0400

    Update App.py

--- App.py
+++ App.py
@@ -197,7 +197,7 @@ BUILDING_VARS = {
     "Rvalue_wall":  {"label": "Wall R-value",             "unit": r"m^2 \cdot K \cdot W^{-1}", "symbol": r"R_{\text{wall}}",   "icon": "🧱"},
     "Glazing":      {"label": "Glazing ratio",            "unit": r"—",                         "symbol": r"GR",                "icon": "🪟"},
     "SHGC":         {"label": "Solar Heat Gain Coeff.",   "unit": r"—",                         "symbol": r"SHGC",              "icon": "🌤️"},
-    "Infiltration": {"label": "Infiltration rate",        "unit": r"h^{-1}",                    "symbol": r"\dot{m}_{\inf}",    "icon": "💨"},
+    "Infiltration": {"label": "Infiltration rate",        "unit": r"ACH",                    "symbol": r"\dot{m}_{\inf}",    "icon": "💨"},
     "Albedo_roof":  {"label": "Roof albedo",              "unit": r"—",                         "symbol": r"\alpha",            "icon": "☀️"},
     "A_PV":         {"label": "PV area ratio",            "unit": r"—",                         "symbol": r"A_{\text{PV}}",     "icon": "⚡"},
     "A_ST":         {"label": "Solar thermal area",       "unit": r"—",                         "symbol": r"A_{\text{ST}}",     "icon": "🌡️"},
